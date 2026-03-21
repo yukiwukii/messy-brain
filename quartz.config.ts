4d40e70c1739fa34c4b19ee441e93b40577aa1e3
yukiwuki <131858939+yukiwukii@users.noreply.github.com>
2026-03-21 10:22:49 +0100

    Quartz sync: Mar 21, 2026, 10:22 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -57,7 +57,7 @@ const config: QuartzConfig = {
     transformers: [
       Plugin.FrontMatter(),
       Plugin.CreatedModifiedDate({
-        priority: ["frontmatter", "git", "filesystem"],
+        priority: ["frontmatter", "filesystem", "git"],
       }),
       Plugin.SyntaxHighlighting({
         theme: {
