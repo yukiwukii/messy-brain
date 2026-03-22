bd4f2c2d718339765b1432eba2ce4e9de587685e
yukiwuki <131858939+yukiwukii@users.noreply.github.com>
2026-03-22 19:02:22 +0100

    Quartz sync: Mar 22, 2026, 7:02 PM

--- quartz/plugins/emitters/folderPage.tsx
+++ quartz/plugins/emitters/folderPage.tsx
@@ -71,7 +71,7 @@ function computeFolderInfo(
       defaultProcessedContent({
         slug: joinSegments(folder, "index") as FullSlug,
         frontmatter: {
-          title: `${i18n(locale).pages.folderContent.folder}: ${folder}`,
+          title: `${folder}`,
           tags: [],
         },
       }),
